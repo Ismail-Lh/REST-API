d77ce8cab373e6777e9619f3b20fa62421b692c2
Ismail-Lh <ismailsaad03@gmail.com>
2023-09-19 19:48:36 +0100

    setup an express server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,23 @@
-console.log("hello world");
-console.log("ncjdnvkn");
+import bodyParser from "body-parser";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+const PORT = 8080;
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.listen(PORT, () =>
+  console.log(`Server running on port http://localhost:${PORT}`)
+);
