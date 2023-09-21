1ef555508aa82cbf4b3b3af25803509ee68bd7f8
Ismail-Lh <ismailsaad03@gmail.com>
2023-09-21 17:19:17 +0100

    add some verification to the User model schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,8 +2,18 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, min: 4, max: 15 },
-    email: { type: String, required: true },
+    username: {
+      type: String,
+      required: [true, "Please enter a unique Username"],
+      min: 4,
+      max: 15,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter an Email Address."],
+      unique: true,
+    },
     password: { type: String, required: true, min: 6, max: 15 },
     active: { type: Boolean, default: true },
   },
