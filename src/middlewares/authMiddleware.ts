a84b2b6c06fd2b78d176f6985f663364a044a7b0
Ismail-Lh <ismailsaad03@gmail.com>
2023-09-22 17:23:46 +0100

    create the checkDuplicateUser middleware (checking for existing username or email)

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Response, Request } from "express";
+import User from "../models/User";
+
+export const checkDuplicateUser = async (req: Request, next: NextFunction) => {
+  const { username, email } = req.body;
+
+  const existingUser = await User.findOne({ $or: [{ username }, { email }] })
+    .collation({ locale: "en", strength: 2 })
+    .lean()
+    .exec();
+
+  // @ts-ignore
+  req.existingUser = existingUser;
+
+  next();
+};
