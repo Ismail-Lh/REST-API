75896e2be02af2772d54ae95d7c256cad801fee0
Ismail-Lh <ismailsaad03@gmail.com>
2023-10-01 16:22:10 +0100

    add the Express custome Request to the available middleware functions

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,10 +1,20 @@
-import { NextFunction, Response, Request } from "express";
-import User from "../models/User";
+import jwt, { type JwtPayload } from "jsonwebtoken";
 
-export const checkDuplicateUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
+import User from "../models/User";
+import { ExpressMiddleware } from "../types/types";
+
+type CheckUserReq = {
+  username: string;
+  email: string;
+};
+
+type PRoutesRes = Record<"message", string>;
+type PRoutesReq = {};
+
+export const checkDuplicateUser: ExpressMiddleware<CheckUserReq> = async (
+  req,
+  res,
+  next
 ) => {
   const { username, email } = req.body;
 
@@ -13,8 +23,34 @@ export const checkDuplicateUser = async (
     .lean()
     .exec();
 
-  // @ts-ignore
   req.existingUser = existingUser;
 
   next();
+};
+
+export const protectedRoutes: ExpressMiddleware<
+  PRoutesReq,
+  PRoutesRes
+> = async (req, res, next) => {
+  const authHeaders = req.headers.authorization;
+
+  if (!authHeaders && !authHeaders?.startsWith("Bearer"))
+    res.status(401).json({
+      message:
+        "Unauthorized user, you are not logged in. Please log in again to get access...",
+    });
+
+  const token: string | undefined = authHeaders.split(" ")[1];
+
+  jwt.verify(
+    token,
+    process.env.ACCESS_TOKEN_KEY,
+    (err: any, payload: JwtPayload) => {
+      if (err) res.status(403).json({ message: err.message });
+
+      req.payload = payload;
+
+      next();
+    }
+  );
 };
