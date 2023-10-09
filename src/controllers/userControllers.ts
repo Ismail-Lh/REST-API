5da03c671694f76e451f7c46dfa77509ed3b77f0
Ismail-Lh <ismailsaad03@gmail.com>
2023-10-09 17:26:10 +0100

    create the sendEmail funcionality with nodemailer

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -79,3 +79,42 @@ export const updatePassword: ExpressMiddleware = async (req, res) => {
     message: `${user.username} your password has successfully updated!`,
   });
 };
+
+// *@desc   --> Forgot password functionality
+// *@route  --> POST /api/v1/users/forgot-password
+// *@access --> public
+export const forgotPassword: ExpressMiddleware = async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) res.status(401).json({ message: "Email address is required!" });
+
+  const user = await User.findOne({ email }).exec();
+
+  if (!user)
+    res.status(404).json({
+      message: "Invalid email address. There is no user with this email.",
+    });
+
+  // @ts-ignore
+  const resetToken = user.createPasswordResetToken();
+  await user.save();
+
+  // 3- Send the generated token to the user's email
+  const resetURL = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/user/forgot-password/${resetToken}`;
+
+  const emailMessage = `Forgot your password? Submit a PATCH request whit your new password and passwordConfirm to this url ${resetURL}.\nIf you didn't forgot your password, please ignore this email.`;
+
+  await sendEmail({
+    userEmail: user.email,
+    subject: "Your password reset token (valid for 10 minute)",
+    emailMessage,
+    emailBody: `Please click <a href="${resetURL}">here</a> to reset your password.`,
+  });
+
+  res.status(200).json({
+    success: "success",
+    message: "Token sent to email address!",
+  });
+};
