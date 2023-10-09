c91fe6116a9b0dce64f256ad51bb8ee8958f3540
Ismail-Lh <ismailsaad03@gmail.com>
2023-10-09 17:28:42 +0100

    add passwordResetToken & passwordResetExpires fields and add the createPasswordResetToken methods to the User's Schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,7 @@
 import { NextFunction } from "express";
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
+import crypto from "crypto";
 
 const userSchema = new mongoose.Schema(
   {
@@ -17,6 +18,8 @@ const userSchema = new mongoose.Schema(
       unique: true,
     },
     password: { type: String, required: true, min: 6, max: 15 },
+    passwordResetToken: String,
+    passwordResetExpires: Date,
     active: { type: Boolean, default: true },
   },
   { timestamps: true }
@@ -29,6 +32,23 @@ userSchema.pre("save", async function (next: NextFunction) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+// !: Password Reset token
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  // !: Reset token expires in 10 minute
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  console.log({ resetToken }, this.passwordResetToken);
+
+  return resetToken;
+};
 
 const User = mongoose.model("User", userSchema);
 
